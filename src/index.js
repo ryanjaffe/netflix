78cd2f8ee2e2f3cb21af59513c21500c8c78c416
Ryan Jaffe <ryancjaffe@gmail.com>
2020-10-25 16:01:58 -0500

    Created folders for containers and refactored the jumbotron to export from containers as a default in the component's index.js file.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
+import 'normalize.css';
 import App from './app';
 import { GlobalStyles } from './global-styles';
 
-render(<>
-    <GlobalStyles />
-    <App />
+render(
+    <>
+        <GlobalStyles />
+        <App />
     </>,
-    document.getElementById('root'));
+    document.getElementById('root')
+);
