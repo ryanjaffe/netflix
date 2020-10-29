49cb2f4dd913cceda215c93635c78f9c3b218c87
Ryan Jaffe <ryancjaffe@gmail.com>
2020-10-29 04:31:53 -0500

    Added movie seed data and the firebase config info for the app.

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -5,7 +5,15 @@ import 'firebase/auth';
 //Need to somehow seed the database
 
 //Need a config here
-const config = {};
+const config = {
+    apiKey: 'AIzaSyCpNAPtUD6CDPvySWn1-FnIjYJMCfuldiE',
+    authDomain: 'netflix-94f3a.firebaseapp.com',
+    databaseURL: 'https://netflix-94f3a.firebaseio.com',
+    projectId: 'netflix-94f3a',
+    storageBucket: 'netflix-94f3a.appspot.com',
+    messagingSenderId: '562543380691',
+    appId: '1:562543380691:web:3dcd5504096f06add42c26'
+};
 
 const firebase = Firebase.initializeApp(config);
 
